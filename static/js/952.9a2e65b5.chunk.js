75f09c44e77ca82b883c357710375bca9bcdbe97
VedPatel21 <vedpatel991115rox@gmail.com>
2025-04-15 09:48:35 +0530

    Updates

---
+++ static/js/952.9a2e65b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkurbanedge_website=self.webpackChunkurbanedge_website||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.9a2e65b5.chunk.js.map
